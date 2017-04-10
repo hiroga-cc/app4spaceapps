51f2efa849b37f141ff2bc5511984b9e3ae9eb91
小笠原寛明 <hiroga1030@gmail.com>
2017-04-11 00:29:27 +0900

    add class for method callign

--- a4saCH.py
+++ a4saCH.py
@@ -19,11 +19,11 @@ class a4saCH():
         self.gen = send.GenJson()
 
     def switch(self,sender,text):
-        if text in ("Yes","True"):
+        if text in ("Yes","Sure"):
             send.send(self.gen.setText(sender, "Me too!!"))
             send.send(self.gen.setOption(sender, "Which category?",self.themes))
         elif text in self.themes:
-            send.send(self.gen.setTestPlaneList(sender, getAppsByTheme(self,text)))
+            send.send(self.gen.setTestPlaneList(sender, self.dao.getAppsByTheme(self,text)))
         else:
-            send.send(self.gen.setOption(sender, "Are you interested in space apps??",["Yes", "True"]))
+            send.send(self.gen.setOption(sender, "Are you interested in space apps??",["Yes", "Sure"]))
         return
