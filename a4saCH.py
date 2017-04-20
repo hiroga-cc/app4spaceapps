67928062b30e279eaba2ee27224b4aa49792cfe8
小笠原寛明 <hiroga1030@gmail.com>
2017-04-21 07:41:28 +0900

    change theme

--- a4saCH.py
+++ a4saCH.py
@@ -12,7 +12,7 @@ channel = a4sa
 
 class a4saCH():
 
-    themes = ["Earth", "Aero", "Mars", "Solar-System", "Space-Station", "Tech"]
+    themes = ["earth", "aero", "mars", "solar-system", "space-station", "tech"]
 
     def __init__(self):
         self.dao = a4saDAO.a4saDAO()
