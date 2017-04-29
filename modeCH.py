d65c3d6ef2a55aed2e66c9e53ec2b00bdec5a836
小笠原寛明 <hiroga1030@gmail.com>
2017-04-30 01:15:10 +0900

    add modech module

---
+++ modeCH.py
@@ -0,0 +1,25 @@
+# coding: UTF-8
+
+import send
+import modeDAO
+
+class modeCH():
+
+    modes = ["SPACE APPS", "INIT"]
+
+    def __init__(self):
+        self.dao = modeDAO.modeDAO()
+        self.gen = send.GenJson()
+
+    def　getMode(self, sender):
+        mode=self.dao.getMode(sender)
+        return mode
+
+    def switch(self,sender,text):
+        if text in self.modes:
+            self.dao.changeMode(sender,mode)
+            send.send(self.gen.setText(sender, "Swithed!"))
+        else:
+            send.send(self.gen.setText(sender, "Hello!!"))
+            send.send(self.gen.setOption(sender, "Select Your Mode",modes))
+        return
