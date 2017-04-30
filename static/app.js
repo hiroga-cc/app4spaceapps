3c35ac04b59d46cd2f764a12ff79d5a0645ccef1
t.hirai <komorebiyori@gmail.com>
2017-04-30 13:29:30 +0900

    Japan message end

--- static/app.js
+++ static/app.js
@@ -4,15 +4,23 @@ $(function () {
 
         $countryId = $(this).data('id');
 
+        
+        console.log($countryId);
+        
         if ($countryId == 'usa') {
             $('.start_modal_trigger_btn').trigger('click');
             return;
         }
 
+        // modal 発火
         $('.modal_trigger_btn').trigger('click');
+
+        // 各国のパネルを表示
+        $('#panel_' + $countryId ).removeClass('hidden');
+
         var msg = getModalText($countryId);
-        var input = getInput();
-        $('p#msg').html(msg);
+        // $('p#msg_' + $countryId).html(msg);
+        $('p.msg').html(msg);
 
     });
 
@@ -28,11 +36,32 @@ $(function () {
     $('.answerRadio').click(function () {
         console.log('radio click');
     });
+    
+    $('.clickable-panel').click(function () {
+        var collect = $(this).data('collect');
+        var child = $(this).children();
+        if (collect === 'y') {
+            // 正誤
+            $(this).addClass('panel-success');
+            child.removeClass('hidden');
+
+            // next botton
+            $('#save').removeClass('hidden');
+
+        } else {
+            // 正誤
+            $(this).addClass('panel-danger');
+            child.removeClass('hidden');
+        }
+        
+    });
+    
+    
 });
 
 
 
-function getInput() {
+// function getInput() {
     // return '
     // <div class="input-group">\
     //   <input type="text" class="form-control" placeholder="answer" aria-describedby="basic-addon1">\
@@ -40,7 +69,7 @@ function getInput() {
     //     <button class="btn btn-primary" id="go" type="button">Go!</button>\
     //   </span>\
     // </div>';
-}
+// }
 
 
 
@@ -51,7 +80,7 @@ function getModalText(countryId) {
             msg = '地震発生時、発生前後の変化をみるにはどうすればいい？';
             break;
         case 'australia':
-            msg = 'australia clicked';
+            msg = '水の少ない場所で、大規模農業をするためには、賢く水を使うのが大切だよ。空の上から、どうすればいい？';
             break;
         case 'southafrica' :
             msg = 'southafrica clicked';
