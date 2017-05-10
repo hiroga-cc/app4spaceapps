dc815c231d10aba02e0cbb1fb12b831f420e2a8b
小笠原寛明 <hiroga1030@gmail.com>
2017-05-11 08:05:50 +0900

    version 3D!!

--- static/app.js
+++ static/app.js
@@ -1,27 +1,6 @@
 $(function () {
     'use strict';
 
-    $('.country').click(function () {
-
-        var $countryId = (this.id);
-
-        // initialize modal
-        initModal();
-
-        if ($countryId == 'usa') {
-            return $('.start_modal_trigger_btn').trigger('click');
-        }
-
-        // modal 発火
-        $('.modal_trigger_btn').trigger('click');
-
-        // 問題文
-        createMsgAndQuestions($countryId);
-
-    });
-
-
-
     // click 選択肢
     $('#panel').on("click", ".click-able-panel", function () {
         var child = $(this).children();
@@ -40,6 +19,21 @@ $(function () {
     });
 });
 
+function showModal(countryId) {
+
+    // initialize modal
+    initModal();
+
+    if (countryId == 'usa') {
+        return $('.start_modal_trigger_btn').trigger('click');
+    }
+
+    // modal 発火
+    $('.modal_trigger_btn').trigger('click');
+
+    // 問題文
+    createMsgAndQuestions(countryId);
+};
 
 /*
  * 問題文生成
@@ -134,11 +128,9 @@ function getModalText(countryId) {
         case 'southafrica' :
             msg = '南極海に船が向かうとき、最も南極に近い南アフリカ共和国などが拠点としてよく使用されるよ。天候の変化が激しい南極海で、安全に航海できるようにしてほしい！';
             break;
-
         case 'germany' :
             msg = 'ヨーロッパでは、環境に優しい海のエネルギーの研究が進められています。海の上の風や波のデータを蓄えるには、どんなSpace Appsを使えばいいかな？';
             break;
-
         case 'chile' :
             msg = '南アメリカへの旅行では、空気が薄い場所での高山病に気をつけないと。体の調子やどれだけ運動したかが一目で分かるようにできないかな？'
             break;
@@ -147,4 +139,167 @@ function getModalText(countryId) {
     return msg;
 }
 
+/* 3D描画 */
 
+var map
+var camera
+var scene, group, mesh
+var mouseX = 0, mouseY = 0;
+var mouse = { x: 0, y: 0 };
+var renderer
+var stat
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+var projector = new THREE.Projector();
+
+// 以下の値はこれから作成する3Dデータのプロパティである。
+var r = 215; //地球の半径
+var c_size = r/15; // 地球の半径に対する、スポットの円のサイズ
+
+// 以下の値は読み込む画像に依存する。
+var map_w = 1280;
+var map_h = 640;
+var map_xy = {
+  "japan":{
+    "x":600,"y":285
+  }, "australia":{
+    "x":640,"y":520
+  }, "sounthafrica":{
+    "x":165,"y":510
+  }, "chile":{
+    "x":1150,"y":525
+  }, "germany":{
+    "x":130,"y":220
+  }, "usa":{
+    "x":1120,"y":300
+  }
+};
+var mesh_ary = [];
+
+init()
+animate()
+
+function init(){
+  map = document.getElementById("map")
+  camera = new THREE.PerspectiveCamera(60, window.innerWidth/ window.innerHeight, 1, 2000)
+  camera.position.z = 500
+
+  scene = new THREE.Scene()
+  group = new THREE.Group()
+  scene.add(group)
+
+  // earth
+  var loader = new THREE.TextureLoader();
+  loader.load( "static/worldmap.png", function ( texture ) {
+    var geometry = new THREE.SphereGeometry( r, 16, 16 ); // 半径、水平切断面の辺の数、垂直切断面の〜
+    var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
+    var mesh = new THREE.Mesh( geometry, material );
+    group.add( mesh );
+
+  } );
+
+
+  var i = 0;
+  for (key in map_xy){
+    i = i+1;
+    // 国別のスポットのメッシュ
+    var mesh_color = 0xe01111;
+    if (key == "usa"){mesh_color = 0x4011ff;}
+    var material = new THREE.MeshBasicMaterial( { color: mesh_color } );
+    var geometry = new THREE.CircleGeometry( c_size, 32 );
+    var mesh = new THREE.Mesh( geometry, material );
+    mesh.name=key;
+
+    var xyz = returnXYZ(key);
+    mesh.position.set(xyz["x"],xyz["y"],xyz["z"]);
+
+    var rad_x = ((map_xy[key]["y"]-(map_h/2))/map_h) * (Math.PI);
+    var rad_y = ((map_xy[key]["x"]-(map_w/4))/map_w) * (2*Math.PI);
+    mesh.rotation.set(rad_x,rad_y,0);  // xはお辞儀、yはメリーゴーランド, zは車輪
+    group.add( mesh );
+    mesh_ary.push( mesh );
+  };
+
+  renderer = new THREE.CanvasRenderer()
+  renderer.setClearColor( 0xffffff );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  map.appendChild( renderer.domElement );
+
+  // stats = new Stats()
+  // map.appendChild(stats.dom)
+  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+  window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function returnXYZ(country){
+  var theta1,theta2; // 上記を元に割り出した、3Dに座標をマップした際の角度
+  var x_,z_; //y=0におけるx,z座標
+  var xyz = {};
+
+  theta1 = map_xy[country]["x"] / (map_w/2) * Math.PI; //ex. 日本ならだいたい3
+  theta2 = map_xy[country]["y"] / map_h * Math.PI; //ex./日本ならだいたい1.5
+  x_ = Math.cos(theta1) * r;
+  z_ = Math.sin(theta1) * r;
+  xyz["x"] = x_ * Math.sin(theta2) * -1; //地図と3Dオブジェクトで座標の開始位置を揃えるため、マイナス反転が必要
+  xyz["y"] = Math.cos(theta2) * r;
+  xyz["z"] = z_ * Math.sin(theta2);
+  return xyz;
+}
+
+function animate() {
+  requestAnimationFrame( animate );
+  render();
+  // stats.update();
+}
+function render() {
+  camera.lookAt( scene.position );
+  group.rotation.x -=  mouseY * 0.0001;
+  group.rotation.y -=  mouseX * 0.0001;
+  renderer.render( scene, camera );
+}
+
+function onWindowResize() {
+  windowHalfX = window.innerWidth / 2;
+  windowHalfY = window.innerHeight / 2;
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function onDocumentMouseMove( event ) {
+  // このmouseXとYは、画面中央からのズレ度合いを示す
+  mouseX = ( event.clientX - windowHalfX );
+  mouseY = ( event.clientY - windowHalfY );
+}
+window.onmousedown = function(ev){
+  if (ev.target == renderer.domElement){
+
+    //マウス座標2D変換
+    var rect = ev.target.getBoundingClientRect();
+    mouse.x =  ev.clientX - rect.left;
+    mouse.y =  ev.clientY - rect.top;
+
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    //マウス座標3D変換 width（横）やheight（縦）は画面サイズ
+    mouse.x =  (mouse.x / width) * 2 - 1;
+    mouse.y = -(mouse.y / height) * 2 + 1;
+
+    // マウスベクトル
+    var vector = new THREE.Vector3( mouse.x, mouse.y ,1);
+
+   // vector はスクリーン座標系なので, オブジェクトの座標系に変換
+    projector.unprojectVector( vector, camera );
+
+    // 始点, 向きベクトルを渡してレイを作成
+    var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+    var obj = ray.intersectObjects(mesh_ary)
+    if (obj.length > 0){
+      showModal(obj[0]['object'].name);
+    }
+  }
+}
